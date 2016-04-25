63013d27e100c5b5519a14eda5448a0a87faa66e
Peter Nied <pnied@microsoft.com>
2016-04-25 15:16:44 -0700

    Fix broken test

--- graphsdk/src/androidTest/java/com/microsoft/graph/serializer/DateOnlyTests.java
+++ graphsdk/src/androidTest/java/com/microsoft/graph/serializer/DateOnlyTests.java
@@ -4,27 +4,27 @@ import android.test.AndroidTestCase;
 
 import com.microsoft.graph.model.DateOnly;
 
-public class DateOnlySerializerTest extends AndroidTestCase {
+public class DateOnlyTests extends AndroidTestCase {
 
     public void testDateSerializer() throws Exception {
-        String strDate = DateOnlySerializer.serialize(DateOnly.parse("2016-04-27"));
+        String strDate = DateOnly.parse("2016-04-27").toString();
         assertEquals("2016-04-27", strDate);
     }
 
     public void testDateSerializerIndefinite() throws Exception {
-        String strDate = DateOnlySerializer.serialize(DateOnly.parse("0001-01-01"));
+        String strDate = DateOnly.parse("0001-01-01").toString();
         assertEquals("0001-01-01", strDate);
     }
 
     public void testDateDeserializer() throws Exception {
-        DateOnly date = DateOnlySerializer.deserialize("2016-04-27");
+        DateOnly date = DateOnly.parse("2016-04-27");
         assertEquals(2016, date.getYear());
         assertEquals(4, date.getMonth());
         assertEquals(27, date.getDay());
     }
 
     public void testDateDeserializerIndefinite() throws Exception{
-        DateOnly date = DateOnlySerializer.deserialize("0001-01-01");
+        DateOnly date = DateOnly.parse("0001-01-01");
         assertEquals(1, date.getYear());
         assertEquals(1, date.getMonth());
         assertEquals(1, date.getDay());
