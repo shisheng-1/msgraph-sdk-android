9dd16a5f590eda93081903a9dedd125607deb492
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-05-23 15:03:05 -0700

    Fix for #53
    Return null when 202 response is received

--- graphsdk/src/main/java/com/microsoft/graph/http/DefaultHttpProvider.java
+++ graphsdk/src/main/java/com/microsoft/graph/http/DefaultHttpProvider.java
@@ -291,6 +291,7 @@ public class DefaultHttpProvider implements IHttpProvider {
 
                 if (connection.getResponseCode() == HttpResponseCode.HTTP_ACCEPTED) {
                     mLogger.logDebug("Handling accepted response");
+                    return null;
                 }
 
                 in = new BufferedInputStream(connection.getInputStream());
